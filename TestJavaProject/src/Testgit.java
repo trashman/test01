f216fcf72bc878c627e0b7f36c59dce8ab67e58a
trahasck <trahasck@RBAMW047665.emea.roche.com>
2016-05-12 15:27:28 +0200

    my next commit

--- TestJavaProject/src/Testgit.java
+++ TestJavaProject/src/Testgit.java
@@ -4,7 +4,7 @@ public class Testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hallo Klaus");
+		System.out.println("Hallo Klaus Trahasch");
 
 	}
 
