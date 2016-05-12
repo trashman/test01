c083fa5eeb5814356b204b386dc7d4886bee9167
trahasck <trahasck@RBAMW047665.emea.roche.com>
2016-05-12 12:16:16 +0200

    2nd commit to master and origin

--- TestJavaProject/src/Testgit.java
+++ TestJavaProject/src/Testgit.java
@@ -4,7 +4,7 @@ public class Testgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hallo");
+		System.out.println("Hallo Klaus");
 
 	}
 
